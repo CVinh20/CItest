b23a76d12038304081b2a0f32c51fe7049614d23
Lê Huỳnh Công Vinh <lehuynhcongvinhc1.c3hn2019@gmail.com>
2026-03-16 15:12:03 +0700

    Test

---
+++ lehuynhcongvinh/src/main/java/com/example/lehuynhcongvinh/LehuynhcongvinhApplication.java
@@ -0,0 +1,13 @@
+package com.example.lehuynhcongvinh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LehuynhcongvinhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LehuynhcongvinhApplication.class, args);
+	}
+
+}
---
+++ lehuynhcongvinh/src/test/java/com/example/lehuynhcongvinh/LehuynhcongvinhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lehuynhcongvinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LehuynhcongvinhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
